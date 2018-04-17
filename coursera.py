b5847f3e784bce623049fc1fb8eb128e3d701b43
yuriy_beloff <paganismrus@gmail.com>
2018-04-17 17:24:18 +0300

    modified function get_course_inform, added dictionary instead of list

--- coursera.py
+++ coursera.py
@@ -6,11 +6,8 @@ import argparse
 import os
 
 
-def get_courses_list(record_count):
-    xml = requests.get(
-        "https://www.coursera.org/sitemap~www~courses.xml"
-    ).content
-    root = etree.fromstring(xml)
+def get_courses_link_list(xml_content, record_count):
+    root = etree.fromstring(xml_content)
     url_list = []
     for element in root.getchildren():
         for child in element.getchildren():
@@ -18,34 +15,57 @@ def get_courses_list(record_count):
     return url_list[:record_count]
 
 
-def get_course_info(course):
-    course_info = []
-    html_content = requests.get(course).content
+def get_content(link):
+    response = requests.get(link).content
+    return response
+
+
+def get_course_inform(html_content, course):
     soup = BeautifulSoup(html_content, 'html.parser')
-    try:
-        course_info.append(soup.find_all('h2')[0].get_text())
-        course_info.append(soup.find_all('div', 'rc-Language')[0].get_text())
-        course_info.append(soup.find_all(
-            'div', 'rc-StartDateString'
-        )[0].get_text())
-        course_info.append(soup.find_all('div', 'rc-BasicInfo')[0].get_text())
-        course_info.append(soup.find_all('div', 'ratings-text')[0].get_text())
-    except IndexError:
-        course_info.append("No data yet")
-    return course_info
+    course_inform = dict(
+        course_title=soup.find_all('h2')[0].get_text(),
+        language=soup.find_all(
+            'div',
+            'rc-Language'
+        )[0].get_text(),
+        start_date=soup.find_all(
+            'div',
+            'rc-StartDateString'
+        )[0].get_text(),
+        continuation=soup.find_all(
+            'div',
+            'rc-BasicInfo'
+        )[0].get_text(),
+        rating=None
+    )
+    rating = soup.find_all(
+        'div',
+        'ratings-text headline-2-text'
+    )
+    if rating:
+        course_inform['rating'] = rating[0].getText()
+    return course_inform
 
 
-def output_courses_info_to_xlsx(course_list, filepath):
-    wb = Workbook()
-    ws = wb.active
+def output_courses_info_to_xls(course_list, ws):
     ws.title = 'Coursera courses info'
-    ws['A1'] = 'Course Title'
-    ws['B1'] = 'Language'
-    ws['C1'] = 'Start Date'
-    ws['D1'] = 'Continouation'
-    ws['E1'] = 'Rating'
+    ws['A1'] = 'course_title'
+    ws['B1'] = 'language'
+    ws['C1'] = 'start_date'
+    ws['D1'] = 'continouation'
+    ws['E1'] = 'rating'
     for course in course_list:
-        ws.append(course)
+        ws.append([
+            course['course_title'],
+            course['language'],
+            course['start_date'],
+            course['continuation'],
+            course['rating']
+        ])
+    return ws
+
+
+def save_work_book(wb, filepath):
     wb.save(filepath)
 
 
@@ -60,13 +80,19 @@ def parse_argument():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     record_count = 20
-    url_list = get_courses_list(record_count)
+    link = 'https://www.coursera.org/sitemap~www~courses.xml'
+    xml_content = get_content(link)
+    url_list = get_courses_link_list(xml_content, record_count)
     course_list = []
     arg = parse_argument()
     filepath = arg.path
-    for each_course in url_list:
-        course_info = get_course_info(each_course)
+    for url in url_list:
+        html_content = get_content(url)
+        course_info = get_course_inform(html_content, url)
         course_list.append(course_info)
-    output_courses_info_to_xlsx(course_list, filepath)
+    wb = Workbook()
+    ws = wb.active
+    output_courses_info_to_xls(course_list, ws)
+    save_work_book(wb, filepath)
